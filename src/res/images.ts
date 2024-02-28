633e0b6a42bb2ae42ce626dc19678e7a16cade36
Ivan Bugaenko <bugaenko1234567890@yandex.ru>
2024-02-28 20:51:56 +0600

    feature(resources): add app images, strings and colors

---
+++ src/res/images.ts
@@ -0,0 +1,5 @@
+const AppImages: { [id: string]: string } = {
+  marvel_logo: String.raw`src\assets\icons\marvel_logo.svg`
+};
+
+export default AppImages;
---
+++ src/res/strings.ts
@@ -0,0 +1,11 @@
+const AppStrings: { [id: string]: string } = {
+  character_link: 'Character',
+  comics_link: 'Comics',
+  character_header: 'Characters',
+  character_search_placeholder: 'Search for Characters by Name',
+  comics_header: 'Comics',
+  comics_search_placeholder: 'Search for Comics by Name',
+  search_button_text: 'SEARCH'
+};
+
+export default AppStrings;
