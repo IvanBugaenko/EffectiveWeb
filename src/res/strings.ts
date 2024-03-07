29831ddeddb8d545184169be13e04b502fcd1cdc
Ivan Bugaenko <bugaenko1234567890@yandex.ru>
2024-03-07 21:55:22 +0600

    fix(resources): fix app strings and paths

--- src/res/strings.ts
+++ src/res/strings.ts
@@ -1,8 +1,8 @@
 const AppStrings: { [id: string]: string } = {
-  character_link: 'Character',
+  characters_link: 'Characters',
   comics_link: 'Comics',
-  character_header: 'Characters',
-  character_search_placeholder: 'Search for Characters by Name',
+  characters_header: 'Characters',
+  characters_search_placeholder: 'Search for Characters by Name',
   comics_header: 'Comics',
   comics_search_placeholder: 'Search for Comics by Name',
   search_button_text: 'SEARCH'
