7879928addce145e773ed2fa0b1d007d6541256a
Ivan Bugaenko <bugaenko1234567890@yandex.ru>
2024-03-24 23:32:19 +0600

    feature(pages): add index.ts file

---
+++ src/routes/Pages/index.ts
@@ -0,0 +1,6 @@
+import CharactersPage from './Characters';
+import ComicsPage from './Comics';
+import CharacterDetailsPage from './CharactersDetails';
+import ComicsDetailsPage from './ComicsDetails';
+
+export { CharactersPage, ComicsPage, CharacterDetailsPage, ComicsDetailsPage };
