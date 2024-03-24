5b28de9a6da32cccc8283989f4feeb9795e4a946
Ivan Bugaenko <bugaenko1234567890@yandex.ru>
2024-03-24 20:56:31 +0600

    feature(types: LinkCard): remove link_cards prop

--- src/types/linkCard.ts
+++ src/types/linkCard.ts
@@ -1,9 +1,6 @@
-import NavigationLink from './navigationLink';
-
 export interface LinkCard {
   id: number;
   name: string;
   image_url: string;
   description: string;
-  linked_cards: NavigationLink[];
 }
