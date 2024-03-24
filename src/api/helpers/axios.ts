cc4cb2424294728f51fc5664cfb4ec454a19b6e3
Ivan Bugaenko <bugaenko1234567890@yandex.ru>
2024-03-24 14:06:34 +0600

    feature(api): add axios helper

---
+++ src/api/helpers/axios.ts
@@ -0,0 +1,43 @@
+import axios from 'axios';
+import envs from '../../config/environments';
+import { Flip, toast } from 'react-toastify';
+
+const instance = axios.create({
+  baseURL: envs.baseApiUrl
+});
+
+instance.interceptors.request.use(
+  (config) => {
+    config.params = {
+      ...config.params,
+      apikey: envs.apiKey,
+      hash: envs.apiHash,
+      ts: envs.apiTs
+    };
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
+
+instance.interceptors.response.use(
+  (response) => {
+    return response;
+  },
+  (error) => {
+    toast.error(error.response.data.message, {
+      position: 'top-right',
+      autoClose: 5000,
+      hideProgressBar: false,
+      closeOnClick: true,
+      pauseOnHover: false,
+      draggable: true,
+      theme: 'light',
+      transition: Flip
+    });
+    return Promise.reject(error);
+  }
+);
+
+export default instance;
