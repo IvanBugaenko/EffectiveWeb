8d66f90b1290d104aa6497493efccd72cf1e6483
Ivan Bugaenko <bugaenko1234567890@yandex.ru>
2024-03-24 13:57:21 +0600

    fix(types): add index.ts file

---
+++ src/api/baseTypes/index.ts
@@ -0,0 +1,13 @@
+import { Image } from './image';
+import { CharacterDataContainer } from './characterDataContainer';
+import { Character } from './character';
+import { ComicsDataContainer } from './comicsDataContainer';
+import { Comics } from './comics';
+
+export type {
+  Image,
+  CharacterDataContainer,
+  Character,
+  ComicsDataContainer,
+  Comics
+};
