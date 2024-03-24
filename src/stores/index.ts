9bd313d35e822f4742a1881303edfaa34ef99ce2
Ivan Bugaenko <bugaenko1234567890@yandex.ru>
2024-03-24 16:06:31 +0600

    feature(store): add index.ts file

---
+++ src/stores/index.ts
@@ -0,0 +1,15 @@
+import { characterComicsStore } from './Characters/CharacterComicsStore';
+import { characterDataContainerStore } from './Characters/CharacterDataContainerStore';
+import { characterStore } from './Characters/CharactersStore';
+import { comicsCharactersStore } from './Comics/ComicsCharacterStore';
+import { comicsDataContainerStore } from './Comics/ComicsDataContainerStore';
+import { comicsStore } from './Comics/ComicsStore';
+
+export {
+  characterComicsStore,
+  characterDataContainerStore,
+  characterStore,
+  comicsCharactersStore,
+  comicsDataContainerStore,
+  comicsStore
+};
